c118adeed809caf144d58686d390c237679c7edb
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2021-06-24 22:51:48 -0400

    Initial Commit

---
+++ src/test/java/com/eureka/schooleurekaserver/schooleurekaserver/SchoolEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.schooleurekaserver.schooleurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
